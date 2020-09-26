0f00e0e6c7cff61380102b355f3e43f164ad734f
Iván E. Hernández T <iamieht@gmail.com>
2020-09-26 12:33:52 +0200

    Update experiments.js
    Control flow

--- Bootcamp_Learning_Path/Introduction_to_JavaScript/Ex1/js/experiments.js
+++ Bootcamp_Learning_Path/Introduction_to_JavaScript/Ex1/js/experiments.js
@@ -32,3 +32,27 @@ const num = 5;
 const num2 = 2.5;
 
 console.log(num, num2);
+
+// Control flow
+
+if (isTheSkyBlue) {
+    console.log("The sky is blue");
+} else {
+    console.log("The sky is not blue");
+}
+
+if (2 + 2 === 4) {
+    console.log("Math still works");
+} else {
+    console.log("Uh, panic?");
+}
+
+const friendsAtYourParty = 10;
+
+if (friendsAtYourParty === 10) {
+    console.log("Cool, now I have all the nachos to myself");
+} else if (friendsAtYourParty <= 4) {
+    console.log("Perfect amount to play some Mario Kart");
+} else {
+    console.log("Woooooo, play the dance music");
+}
