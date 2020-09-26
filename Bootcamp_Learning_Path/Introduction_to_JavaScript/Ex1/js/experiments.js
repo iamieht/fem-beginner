3e0629f1df0bc305b493c60c1fdd960215b2cb14
Iván E. Hernández T <iamieht@gmail.com>
2020-09-26 11:59:27 +0200

    Update experiments.js
    Template Strings

--- Bootcamp_Learning_Path/Introduction_to_JavaScript/Ex1/js/experiments.js
+++ Bootcamp_Learning_Path/Introduction_to_JavaScript/Ex1/js/experiments.js
@@ -2,3 +2,20 @@ const monthlyRent = 500;
 const yearlyRent = monthlyRent * 12;
 
 console.log(yearlyRent);
+
+// Strings
+
+const myName = "John Doe";
+console.log(myName);
+
+const firstName = "John";
+const lastName = "Doe";
+const sentence = "Hello " + firstName + " " + lastName + "! How are you?";
+
+console.log(sentence);
+
+// Template Strings
+
+const templateSentence = `Hello ${firstName} ${lastName}`;
+
+console.log(templateSentence);
