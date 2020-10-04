a5bcdfc36f61d71ecfd64247c4eaf55538da6fc3
Iván E. Hernández T <iamieht@gmail.com>
2020-10-04 16:12:16 +0200

    Project#4 - Final version

--- Bootcamp_Learning_Path/8.Using_JavaScript_in_Websites/Project#4-dogApp/js/doggos.js
+++ Bootcamp_Learning_Path/8.Using_JavaScript_in_Websites/Project#4-dogApp/js/doggos.js
@@ -1,23 +1,48 @@
-const BREEDS_URL = "https://dog.ceo/api/breeds/list/all";
+const main = document.getElementById("main");
+const loader = document.getElementById("loader");
+const breedSelect = document.getElementById("breed");
 
-function addDoggo() {
-    fetch(BREEDS_URL)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            const breedsObject = data.message;
-            const breedsArray = Object.keys(breedsObject);
-            const select = document.querySelector('.breeds');
 
-            for (let i = 0; i < breedsArray.length; i++) {
-                const option = document.createElement('option');
-                option.value = breedsArray[i];
-                option.innerText = breedsArray[i];
-                select.appendChild(option);
-            }
-        })
-
-}
-
-document.querySelector('.add-doggo').addEventListener('click', addDoggo);
+async function init() {
+    // populate breed list
+    const res = await fetch("https://dog.ceo/api/breeds/list/all");
+    const resJson = await res.json();
+  
+    let breedOptions = "<option></option>";
+  
+    let breedList = Object.keys(resJson.message);
+  
+    for (let i = 0; i < breedList.length; i++) {
+      breedOptions += `<option value=${breedList[i]}>${breedList[i]}</option>`;
+    }
+  
+    breedSelect.innerHTML = breedOptions;
+  
+    // get first image
+    const randomRes = await fetch("https://dog.ceo/api/breeds/image/random");
+    const randomResJson = await randomRes.json();
+  
+    main.src = randomResJson.message;
+  
+    // add event listeners
+    breedSelect.addEventListener("change", handleBreedChange);
+  
+    main.addEventListener("load", function() {
+      main.classList.add("show");
+      loader.classList.remove("show");
+    });
+  }
+  
+  async function handleBreedChange(event) {
+    const breed = event.target.value;
+  
+    main.classList.remove("show");
+    loader.classList.add("show");
+  
+    const res = await fetch(` https://dog.ceo/api/breed/${breed}/images/random`);
+    const resJson = await res.json();
+  
+    main.src = resJson.message;
+  }
+  
+  init();
