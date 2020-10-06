918e40fe289e0d710a0803b339e610a7d59f45c8
Iván E. Hernández T <iamieht@gmail.com>
2020-10-06 18:22:59 +0200

    Create game.js
    Initial commit - Planning Mole statuses

---
+++ Bootcamp_Learning_Path/9.Game_Project_Feed-A-Star-Mole/js/game.js
@@ -0,0 +1,66 @@
+function getSadInterval() {
+    return Date.now() + 1000;
+}
+
+const moles = [
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById('hole-0')
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById('hole-1')
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById('hole-2')
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById('hole-3')
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById('hole-4')
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById('hole-5')
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById('hole-6')
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById('hole-7')
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById('hole-8')
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById('hole-9')
+    }
+]
